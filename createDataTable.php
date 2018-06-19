f3f3a8b77f2c7c17090522627f53447d9f379f9c
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-06-19 13:49:41 +0530

    Updated createDataTable.php

--- createDataTable.php
+++ createDataTable.php
@@ -1,6 +1,10 @@
 <?php
-/*
-$username = $_SESSION['username'];
+
+if(!isset($_SESSION)){ 
+    session_start(); 
+} 
+include_once("connect.php");
+$username = $_SESSION["username"];
 
 $sql = "USE Scheduler;";
 $conn->query($sql);
@@ -9,14 +13,17 @@ $tableName = $username."appointments";
 
 $sql = "CREATE TABLE IF NOT EXISTS $tableName(
 		id INT(100) NOT NULL AUTO_INCREMENT,
-		TaskNumber INT(100) NOT NULL,
-		Checked VARCHAR(200) NOT NULL,
-		TaskText VARCHAR(8000) NOT NULL,
-		Starred VARCHAR(200) NOT NULL,
-		EditTime VARCHAR(1000) NOT NULL,
-		CreateTime VARCHAR(1000) NOT NULL,
-		PRIMARY KEY (id,TaskNumber)
+		AppointmentDate DATE NOT NULL,
+		Title VARCHAR(500) NOT NULL,
+		Description VARCHAR(500) NOT NULL, 
+		FromTime TIME NOT NULL,
+		ToTime TIME NOT NULL,
+		PRIMARY KEY (id,AppointmentDate)
 		)";
-$conn->query($sql);
-*/
+$result = $conn->query($sql);
+
+if (!$result) {
+	trigger_error('Invalid query: ' . $conn->error);
+}	
+
 ?>
