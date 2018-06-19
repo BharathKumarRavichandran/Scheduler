0a0e9ba469c4bcf0ddf808a478a4fd697b0b6cb7
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-06-20 02:53:58 +0530

    User's login check added

--- saveAppointmentData.php
+++ saveAppointmentData.php
@@ -5,6 +5,11 @@ if(!isset($_SESSION)){
     session_start(); 
 } 
 
+if(!isset($_SESSION["username"])){
+	header('Location: login.php');
+	exit();
+}
+
 include_once("connect.php");
 $_SESSION['message']="";
 
